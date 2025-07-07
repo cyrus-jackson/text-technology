5daa8b2f9633ec7123c7c454544e25f992ecd329
dwarkesh12 <73485628+dwarkesh12@users.noreply.github.com>
2025-07-07 11:28:23 +0200

    Update firebase_runner.py
    Update runner with electricity prices and consumption forecast stats

--- Runners/germany_stats/firebase_runner.py
+++ Runners/germany_stats/firebase_runner.py
@@ -2,7 +2,8 @@ import re
 import requests
 import logging
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 from google.oauth2 import service_account
 from google.cloud import firestore
@@ -12,6 +13,11 @@ from lxml import html
 logging.basicConfig(level=logging.INFO)
 todays_date = str(datetime.now().strftime("%Y-%m-%d"))
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+today = datetime.now(GERMAN_TIMEZONE).replace(hour=0, minute=0, second=0, microsecond=0)
+today_ms = int(today.timestamp() * 1000)
+tomorrow_ms = today_ms + 86400000
+# yesterday_ms = today_ms - 86400000
 
 def copy_data_to_firestore(json_data, collection_name):
 
@@ -67,9 +73,57 @@ def fetch_and_parse_fuel_prices():
 
     return prices
 
+def fetch_and_parse_electricity_prices():
+    url = f"https://www.smard.de/app/chart_data/4169/DE/4169_DE_day_1735686000000.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    scraped_data = data.get('series', [])
+
+    processed_data_dict = {}
+
+    for point in scraped_data:
+        timestamp_ms, price_eur_mwh = point
+
+        if today_ms == timestamp_ms:
+            processed_data_dict["today_price"] = price_eur_mwh
+            continue
+
+        if tomorrow_ms == timestamp_ms:
+            processed_data_dict["day_ahead_price"] = price_eur_mwh
+            break
+
+    print(f"Processed data points for tomorrow. Result: {processed_data_dict}")
+    return processed_data_dict
+
+def fetch_and_parse_electricity_forecast():
+    url = f"https://www.smard.de/app/chart_data/411/DE/411_DE_day_1735686000000.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    scraped_data = data.get('series', [])
+
+    processed_data_dict = {}
+
+    for point in scraped_data:
+        timestamp_ms, grid_load_MWh = point
+
+        if tomorrow_ms == timestamp_ms:
+            processed_data_dict["consumption_forecast"] = grid_load_MWh
+            break
+
+    print(f"Processed elec forecast for tomorrow. Result: {processed_data_dict}")
+    return processed_data_dict
+
 if __name__ == '__main__':
     fuel_prices = retry_logic(3, 5, fetch_and_parse_fuel_prices)
     print(fuel_prices)
     copy_data_to_firestore(fuel_prices, "fuel_prices")
+
+    electricity_prices = retry_logic(3, 5, fetch_and_parse_electricity_prices)
+    # consumption = retry_logic(3, 5, fetch_and_parse_electricity_consumption)
+    forecast_consumption = retry_logic(3, 5, fetch_and_parse_electricity_forecast)
+
+    store_data_using_general_function(db, electricity_prices | forecast_consumption, 'electricity')
     # for fuel, val in prices.items():
     #     print(f"{fuel}: € {val:.3f}")
